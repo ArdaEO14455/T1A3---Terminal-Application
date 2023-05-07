935fe48d15d902b19d6eacd2d4b0eac1703f0c01
ArdaEO14455 <14455@coderacademy.edu.au>
2023-05-07 12:47:31 +1000

    "Added Preliminary Pseudocode

---
+++ classes.py
@@ -0,0 +1,35 @@
+# Shared Stats:
+    # HP
+    # damage multiplier
+    # crit chance
+
+# Troll Class(Self, HP (100), Damage multiplier, Crit chance)
+
+    # Def Basic Attack 1
+        # 15 dmg
+        # print("troll did attack 1 at the Adventurer")
+    
+    # Def ttack 2 - Damage Over time (if applicable)
+        # 5 dmg per turn
+        # print("troll applied attack 2 DoT at the Adventurer")
+    
+    # Def Buff (Roar)
+        # + x to damage multiplier, + x% to crit chance for x turns
+        # print("troll roared, preparing to fight harder")
+        # priint()
+    
+
+# Adventurer Class(Self, HP (60), Damage multiplier, crit chance)
+    # Def Basic Attack 1 - Basic Attack
+    # print ("adventurer attacked the troll")
+
+    # Def Attack 2 - Damage Over time (if applicable) 
+    # print ("adventurer applied DoT to the troll")
+
+    # Def Buff (potion of strength)
+    # Print('Adventurer Drank potion of strength)
+    # Print('Your attacks hit harder for two turns)
+
+    # Def Healing Potion
+    # Print('Adventurer Drank potion of healing, revitalising them))
+
---
+++ main.py
@@ -0,0 +1,16 @@
+# Fight The Troll
+
+
+#Start with Introduction + Ask for Character Name
+# While Loop to create back and forth between troll and adventurer until one reaches 0 hp
+# While Troll HP > 0 and Adventurer HP > 0:
+    # Adventurer Turn
+    # Print Action
+    # Print Result
+    # 1s delay
+
+    # Troll Turn
+    # Print Action
+    # Print Result
+    # 1s delay
+
