7ee1382ad30b67eeeffaf6f9304dcb988e2ab7a5
ArdaEO14455 <14455@coderacademy.edu.au>
2023-05-07 20:29:04 +1000

    Created character class + implemented basic combat

--- classes.py
+++ classes.py
@@ -1,23 +1,58 @@
 # Shared Stats:
+class Stats:
+    def __init__(self, dmgmult, critchance):
+        self.dmgmult = dmgmult
+        self.critchance = critchance
     # HP
     # damage multiplier
     # crit chance
 
-# Troll Class(Self, HP (100), Damage multiplier, Crit chance)
 
-    # Def Basic Attack 1
+class Character:
+    def __init__(self, hp, name):
+        self.name = name
+        self.hp = hp
+        self.stats = Stats(0, 0)
+        
+     
+#     def __init__(self, name,):
+#             self.name = name
+#             self.stats = Stats(0, 0, 0)
+    def attack1(self, damage):
+        self.damage = damage
+
+    # def set(self, hp, dmgmult, critchance):
+    #     self.hp = hp
+    #     self.dmgmult = dmgmult
+    #     self.critchance = critchance
+
+    def attack(self, other):
+        print(f'{self.name} does attack 1 at {other.name}')
+        Character.attack1.damage = 10 * (self.stats[0])
+        print(f'{other.name} takes {Character.attack1.damage} Damage')
+        
+
+    # def battle(self, other):
+         
+    #     print(f'dealing {Character.attack.damage} damage')
+
+    
+            
+    # def attack2(self, dmg):
+    #     pass
+
+        # Def Basic Attack 1
         # 15 dmg
         # print("troll did attack 1 at the Adventurer")
     
-    # Def ttack 2 - Damage Over time (if applicable)
+        # Def ttack 2 - Damage Over time (if applicable)
         # 5 dmg per turn
         # print("troll applied attack 2 DoT at the Adventurer")
     
-    # Def Buff (Roar)
+        # Def Buff (Roar)
         # + x to damage multiplier, + x% to crit chance for x turns
         # print("troll roared, preparing to fight harder")
         # priint()
-    
 
 # Adventurer Class(Self, HP (60), Damage multiplier, crit chance)
     # Def Basic Attack 1 - Basic Attack
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Fight The Troll
-
+import random
+import classes
 
 #Start with Introduction + Ask for Character Name
 # While Loop to create back and forth between troll and adventurer until one reaches 0 hp
@@ -14,3 +15,17 @@
     # Print Result
     # 1s delay
 
+
+
+troll = classes.Character('Troll', 100)
+troll.stats = (2, 0.1) 
+
+print(troll.__dict__)
+
+adventurer = classes.Character(
+    input ('What is your Adventurer name?' ), 100) 
+adventurer.stats = (1, 0.1)
+print (adventurer.__dict__)
+
+troll.attack(adventurer)
+print (adventurer.__dict__)
